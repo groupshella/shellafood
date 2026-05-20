028817eb173a1b37033bf5e218883e624e60d26e
nasser1207 <nwsir123@gmail.com>
2026-05-20 14:52:49 +0300

    feat: pass initial categories through CategoryPage to CategoryView

--- src/features/categories/components/category-details/CategoryPage.tsx
+++ src/features/categories/components/category-details/CategoryPage.tsx
@@ -9,15 +9,18 @@ import { EmptyState } from "../shared";
 import { useLanguage } from "@/providers/LanguageProvider";
 import CategoriesGridSkeleton from "../category-list/CategoriesSkeleton";
 import { StoreList } from "../../types/store.types";
+import { Category } from "../../types/category.types";
+import { ApiCategory } from "../../types/api-category.types";
 
 interface CategoryPageProps {
 	initialStoreList: StoreList;
 	moduleId: number;
 	initialPage: number;
 	initialLimit: number;
-  }
+	initialCategories: ApiCategory[];
+}
 
-export default function CategoryPage({ initialStoreList, moduleId, initialPage, initialLimit }: CategoryPageProps) {
-	
-	return <CategoryView initialStoreList={initialStoreList} moduleId={moduleId} initialPage={initialPage} initialLimit={initialLimit} />;
+export default function CategoryPage({ initialStoreList, moduleId, initialPage, initialLimit, initialCategories }: CategoryPageProps) {
+
+	return <CategoryView initialStoreList={initialStoreList} moduleId={moduleId} initialPage={initialPage} initialLimit={initialLimit} initialCategories={initialCategories} />;
 }
