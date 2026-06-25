4e396ff4e480eaed65bcca36bb547c8c620c3497
RashedKlo <rashed.klo.dev@gmail.com>
2026-06-25 04:04:21 +0300

    fix: remove import file in CategoryTabsSkeleton

--- features/hyper-market/Categories/components/sections/CategoryTabs/skeleton.tsx
+++ features/hyper-market/Categories/components/sections/CategoryTabs/skeleton.tsx
@@ -1,10 +1,9 @@
-import { CATEGORY_TABS_HEIGHT_PX } from "./CategoryTabsClient";
 
 export default function CategoryTabsSkeleton() {
     return (
         <div
             className="sticky top-0 z-50 flex items-center gap-2 overflow-hidden bg-[#30913F] px-4 sm:px-5"
-            style={{ height: CATEGORY_TABS_HEIGHT_PX }}
+            style={{ height: 44 }}
         >
             {Array.from({ length: 5 }).map((_, i) => (
                 <div
