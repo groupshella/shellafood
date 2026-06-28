c44d31c1a48545bf780f58dd00af47b75dcf20fd
RashedKlo <rashed.klo.dev@gmail.com>
2026-06-28 20:10:08 +0300

    fix error building brands version

--- features/hyper-market/Brands/api/brands.ts
+++ features/hyper-market/Brands/api/brands.ts
@@ -1,12 +1,13 @@
 import { Brand, GetBrandsResponse } from "@/features/hyper-market/Brands/types/brands.types";
 
 export async function getBrands(moduleId: string): Promise<Brand[]> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v2/brands`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/brands`, {
         method: "GET",
         headers: {
             Accept: "application/json",
             "X-Localization": "ar",
             moduleId,
+            zoneId: process.env.ZONE_ID!,
         },
         next: {
             revalidate: Number(process.env.REVALIDATE_TIME) || 3600,
